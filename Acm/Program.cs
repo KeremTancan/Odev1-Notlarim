61a45d0cb8479cd4d2278b810b232fa8af13b388
Kerem Tancan <tancankerem149@gmail.com>
2023-01-21 23:01:38 +0300

    ilk commit atıldı

---
+++ Acm/Program.cs
@@ -0,0 +1,39 @@
+﻿char yanıt;
+do
+{
+    Random x = new Random();
+    int s, tahmin, i;
+    s = x.Next(100) + 1;
+
+
+
+    Console.WriteLine("Tuttuğum sayıyı tahmin et");
+
+
+    for (i = 1; i <= 5; i++)
+    {
+
+        tahmin = Convert.ToInt32(Console.ReadLine());
+
+        if (tahmin == s)
+        {
+            Console.WriteLine("Tuttuğum sayıyı {0}. denemede buldun", i);
+        }
+        if (tahmin > s)
+        {
+            Console.WriteLine("Daha aşağı");
+        }
+        if (tahmin < s)
+        {
+            Console.WriteLine("Daha yukarı");
+        }
+
+    }
+    Console.WriteLine("Tuttuğum sayı {0} idi", s);
+    Console.WriteLine("Tekrar oynamak ister misin? (e/h)");
+    yanıt = Convert.ToChar(Console.ReadLine());
+
+
+
+} while (yanıt == 'e' || yanıt == 'E');
+		
